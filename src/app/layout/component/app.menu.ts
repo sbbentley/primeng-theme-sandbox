6688ccbc5a21161476f9cffaab70f1d8504d610d
sbbentley <sbentley@isolvedhcm.com>
2025-05-06 23:32:57 -0400

    Final cleanup for cards

--- src/app/layout/component/app.menu.ts
+++ src/app/layout/component/app.menu.ts
@@ -49,42 +49,42 @@ export class AppMenu {
                 icon: 'pi pi-fw pi-briefcase',
                 routerLink: ['/pages'],
                 items: [
-                    {
-                        label: 'Auth',
-                        icon: 'pi pi-fw pi-user',
-                        items: [
-                            {
-                                label: 'Login',
-                                icon: 'pi pi-fw pi-sign-in',
-                                routerLink: ['/auth/login']
-                            },
-                            {
-                                label: 'Error',
-                                icon: 'pi pi-fw pi-times-circle',
-                                routerLink: ['/auth/error']
-                            },
-                            {
-                                label: 'Access Denied',
-                                icon: 'pi pi-fw pi-lock',
-                                routerLink: ['/auth/access']
-                            }
-                        ]
-                    },
+                    // {
+                    //     label: 'Auth',
+                    //     icon: 'pi pi-fw pi-user',
+                    //     items: [
+                    //         {
+                    //             label: 'Login',
+                    //             icon: 'pi pi-fw pi-sign-in',
+                    //             routerLink: ['/auth/login']
+                    //         },
+                    //         {
+                    //             label: 'Error',
+                    //             icon: 'pi pi-fw pi-times-circle',
+                    //             routerLink: ['/auth/error']
+                    //         },
+                    //         {
+                    //             label: 'Access Denied',
+                    //             icon: 'pi pi-fw pi-lock',
+                    //             routerLink: ['/auth/access']
+                    //         }
+                    //     ]
+                    // },
                     {
                         label: 'Crud',
                         icon: 'pi pi-fw pi-pencil',
                         routerLink: ['/pages/crud']
                     },
-                    {
-                        label: 'Not Found',
-                        icon: 'pi pi-fw pi-exclamation-circle',
-                        routerLink: ['/pages/notfound']
-                    },
-                    {
-                        label: 'Empty',
-                        icon: 'pi pi-fw pi-circle-off',
-                        routerLink: ['/pages/empty']
-                    }
+                    // {
+                    //     label: 'Not Found',
+                    //     icon: 'pi pi-fw pi-exclamation-circle',
+                    //     routerLink: ['/pages/notfound']
+                    // },
+                    // {
+                    //     label: 'Empty',
+                    //     icon: 'pi pi-fw pi-circle-off',
+                    //     routerLink: ['/pages/empty']
+                    // }
                 ]
             },
             {
@@ -139,12 +139,12 @@ export class AppMenu {
                         icon: 'pi pi-fw pi-book',
                         routerLink: ['/documentation']
                     },
-                    {
-                        label: 'View Source',
-                        icon: 'pi pi-fw pi-github',
-                        url: 'https://github.com/primefaces/sakai-ng',
-                        target: '_blank'
-                    }
+                    // {
+                    //     label: 'View Source',
+                    //     icon: 'pi pi-fw pi-github',
+                    //     url: 'https://github.com/primefaces/sakai-ng',
+                    //     target: '_blank'
+                    // }
                 ]
             }
         ];
